def63dd679dcf75199eb7ed2f1649364cfa694cd
ercain03 <ercain3@gmail.com>
2016-10-18 18:10:57 -0700

    finished part 2 of the lesson

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Node_and_HTTP
+# Simple Http Server
+To run:
+```
+clone down repo to local
+```
+```
+cd into repo
+```
+Run server:
+```
+node server-2.js
+```
+Make GET request:
+```
+localhost:3000/index.html
+localhost:3000/aboutus.html
+```
---
+++ server-2.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+console.log('Request for ' + req.url + ' by method ' + req.method);
+
+if (req.method == 'GET') {
+
+  let fileUrl;
+
+  if (req.url == '/') fileUrl = '/index.html';
+  else fileUrl = req.url;
+
+    let filePath = path.resolve('./public'+fileUrl);
+
+    let fileExt = path.extname(filePath);
+
+    if (fileExt == '.html') {
+      fs.exists(filePath, (exists) => {
+
+        if (!exists) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end('<h1>Error 404: ' + fileUrl + ' not found</h1>');
+          return;
+        }
+          res.writeHead(200, { 'Content-Type': 'text/html'});
+          fs.createReadStream(filePath).pipe(res);
+      });
+    }
+    else {
+      res.writeHead(404, { 'Content-Type': 'text/html'});
+      res.end('<h1>Error 404: ' + fileUrl + ' not a HTML file</h1>');
+    }
+  }
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Error 404: ' + req.method + ' not supported</h1>');
+  }
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
